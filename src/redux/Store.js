4775e2dbb013fe6b360d7ca576b931f2ec468879
Rishab Kumar Jha <138858208+Rishab87@users.noreply.github.com>
2024-01-06 22:33:31 +0530

    Update Store.js

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -3,6 +3,6 @@ import { CartSlice } from "./Slices/cartSlice";
 
 export const store  = configureStore({
     reducer:{
-        cart: CartSlice.reducer, //idhr .reducer lihkne se product mai jo cart main error aa rha tha woh kaise hta reducer ka matlab kya hai idhr kya ho rha hai isse?
+        cart: CartSlice.reducer,
     }
 });
